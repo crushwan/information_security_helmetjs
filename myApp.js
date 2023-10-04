ebb82b286cad665e750f566be8f68855e6f49009
crushwan <crushwan@gmail.com>
2023-10-04 10:43:57 +0800

    "finish"

--- myApp.js
+++ myApp.js
@@ -2,7 +2,54 @@ const express = require("express");
 const app = express();
 var helmet = require("helmet");
 
+app.use(helmet());
 app.use(helmet.hidePoweredBy());
+
+// Latest helmet.js
+// app.use(
+//   helmet({
+//     xPoweredBy: false,
+//   })
+// );
+
+app.use(helmet.frameguard({ action: "deny" }));
+
+// Latest helmet.js
+// app.use(
+//   helmet({
+//     xFrameOptions: { action: "deny" },
+//   })
+// );
+
+app.use(helmet.xssFilter());
+
+// Latest helmet.js
+// app.use(
+//   helmet({
+//     xXssProtection: true,
+//   })
+// );
+
+app.use(helmet.noSniff());
+
+app.use(helmet.ieNoOpen());
+
+ninetyDaysInSeconds = 90 * 24 * 60 * 60;
+
+app.use(helmet.hsts({ maxAge: ninetyDaysInSeconds, force: true }));
+
+app.use(helmet.dnsPrefetchControl());
+
+app.use(helmet.noCache());
+
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: ["'self'"],
+      scriptSrc: ["'self'", "trusted-cdn.com"],
+    },
+  })
+);
 
 module.exports = app;
 const api = require("./server.js");
